204ce2d046339ea35c1055afabc3f903dffa7288
ext_sebastian.lazarte <ext_sebastian.Lazarte@globalprocessing.com.ar>
2025-12-02 17:33:59 -0300

    login con contraseña

---
+++ photography-portfolio/src/config/auth.ts
@@ -0,0 +1,7 @@
+export const ADMIN_USERNAME = 'slazarte';
+export const ADMIN_PASSWORD_HASH = 'c61370b9271e8af22c3888466105d6b9cdd113181847247d54e08f3d4d00e85c';
+
+export const ADMIN_PROFILE = {
+  name: 'Seba Lazarte',
+  email: 'sebalazarte@gmail.com'
+};
